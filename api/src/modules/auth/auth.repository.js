b22331f819ca73b1441528af83735a3afe1804c1
Nihal Uikey <nihaluikey6488@gmail.com>
2026-06-29 23:48:07 +0530

    feat: add UserService to handle user creation logic and bridge to AuthModel repository

--- api/src/modules/auth/auth.repository.js
+++ api/src/modules/auth/auth.repository.js
@@ -1,5 +1,22 @@
+import User from "./auth.model.js";
+
 class AuthRepository {
   // Domain A will add auth persistence methods here.
+  async createUser(userData) {
+    return await User.create(userData);
+  }
+
+  async findUserByEmail(email) {
+    return await User.findOne({ email });
+  }
+
+  async updateRefreshToken(userId, refreshToken) {
+    return await User.findByIdAndUpdate(
+      userId,
+      { refreshToken },
+      { new: true },
+    );
+  }
 }
 
 export { AuthRepository };
--- api/src/modules/auth/auth.service.js
+++ api/src/modules/auth/auth.service.js
@@ -1,5 +1,53 @@
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
+import { AuthRepository } from "./auth.repository.js";
+import { AppError } from "../../shared/errors/AppError.js";
+import { EnvConfig } from "../../config/env.js";
+
 class AuthService {
   // Domain A will add auth business rules here.
+  constructor(authRepository) {
+    this.authRepository = authRepository || new AuthRepository();
+  }
+
+  async createUser(userData) {
+    const email = userData?.emails?.[0]?.value;
+    if (!email) {
+      throw new AppError("Email is required to create a user", 400);
+    }
+
+    let dbUser = await this.authRepository.findUserByEmail(email);
+    if (!dbUser) {
+      dbUser = await this.authRepository.createUser({
+        email,
+        picture: userData?.photos?.[0]?.value,
+        name: userData?.displayName || "",
+      });
+    }
+
+    const payload = {
+      id: dbUser._id,
+      email: dbUser.email,
+      name: dbUser.name,
+      picture: dbUser.picture,
+    };
+
+    const accessToken = jwt.sign(payload, EnvConfig.get("JWT_ACCESS_SECRET"), {
+      expiresIn: EnvConfig.get("JWT_ACCESS_EXPIRES_IN"),
+    });
+
+    const refreshToken = crypto.randomBytes(64).toString("hex");
+    const userWithToken = await this.authRepository.updateRefreshToken(
+      dbUser._id,
+      refreshToken,
+    );
+
+    return {
+      user: userWithToken,
+      accessToken,
+      refreshToken,
+    };
+  }
 }
 
 export { AuthService };
