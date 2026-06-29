ed903c3dab61b8ee87675bf95bcea2d4af482a68
Nihal Uikey <nihaluikey6488@gmail.com>
2026-06-30 00:03:50 +0530

    feat: add GoogleAuthService to handle external authentication and user synchronization

--- api/src/modules/auth/auth.controller.js
+++ api/src/modules/auth/auth.controller.js
@@ -1,6 +1,29 @@
+import {AuthService} from "./auth.service.js";
+import {EnvConfig} from "../../config/env.js";
 class AuthController {
-  // Domain A will add Google auth handlers here.
+constructor(authService) {
+   this.authService = new AuthService();
+
 }
+
+async GoogleCallBack(req,res){
+const {user}=await this.authService.createUser(req.body)
+res.cookie("accessToken",user.accessToken,{
+httpOnly:true,
+secure:EnvConfig.get("NODE_ENV")==="production",
+sameSite:"strict",
+maxAge:24*60*60*1000
+})
+res.cookie("refreshToken",user.refreshToken,{
+httpOnly:true,
+secure:EnvConfig.get("NODE_ENV")==="production",
+sameSite:"strict",
+maxAge:7*24*60*60*1000
+})
+res.redirect(`${EnvConfig.get("FRONTEND_URL")}/auth/success?accessToken=${user.accessToken}&refreshToken=${user.refreshToken}`)
+}
+}
+
 
 export { AuthController };
 export default AuthController;
